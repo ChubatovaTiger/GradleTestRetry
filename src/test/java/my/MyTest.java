2e73077a8e8fc08a39a4dd9e0fe6dcc908a7d631
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2026-01-19 22:17:07 +0100

    Update and rename mytest10.java to MyTest.java

---
+++ src/test/java/my/MyTest.java
@@ -0,0 +1,25 @@
+package my;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import java.lang.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyTest {
+
+    @Test
+    public void test42s() {
+        // Read system property
+        String failTest = System.getProperty("failTest", "false");
+
+        if (Boolean.parseBoolean(failTest)) {
+            Assert.fail("Test failed because failTest=true");
+        }
+
+        // Otherwise, the test passes
+        System.out.println("Test passed!");
+    }
+}
