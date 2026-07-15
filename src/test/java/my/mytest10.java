ce40fbea306652a14b6a427a833b0fe3fa9561df
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2026-07-15 13:27:51 +0200

    Update mytest10.java

--- src/test/java/my/mytest10.java
+++ src/test/java/my/mytest10.java
@@ -12,7 +12,7 @@ public class mytest10 {
         if (System.currentTimeMillis()%2==1){
         }
         else {
-           // Assert.fail("fails with probability 50%");
+            Assert.fail("fails with probability 50%");
 
         };
 
